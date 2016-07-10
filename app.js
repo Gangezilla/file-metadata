a74061c71a33f4469bc34c16706c99012dc282a9
Scott <scottgangemi@gmail.com>
2016-07-10 21:33:24 +1000

    file uploading works with molter, can view file sizes in console.

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 var express=require('express');
 var app=new express();
+var multer=require('multer');
+
+var upload=(multer({dest:'./uploads/'}));
 
 app.set('port', (process.env.PORT || 8080));
 app.listen(app.get('port'), function() {
     console.log("Listening on " + app.get('port') + ".");
 });
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/templates');
+
 app.get("/", function(req,res) {
-	res.send("<h1> this is a test to make sure this deploys");
+	res.render('index');
 });
+
+app.post('/', multer({
+	 dest: './uploads/',
+	 limits: {fileSize:5000000, files:1}
+	}).single('fileUpload'), function(req,res){
+	console.log(req.body);
+	console.log(req.file); 
+	res.status(204).end();
+});
+
+//need to handle large file uploads, throw back an error instead of that screen, maybe?
+//also, probs dont want to store every single file on here. so, probably get the data, send filesize back, then remove the file from the  uploads folder once we're done with it.
+
+
+// User Story: I can submit a FormData object that includes a file upload. CHECK
+
+// User Story: When I submit something, I will receive the file size in bytes within the JSON response
+
+//Personal Story: Files will be removed after download so as to prevent big crazy server crashes.
+
+//Personal Story: Display an error, and allow a user to upload a smaller file, if the file size is too large.
+
+//little html page with an upload button, that when yo
